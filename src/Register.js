d258019836948bec538ea045339817160fefc555
wanasyraf96 <wanasyraf96@yahoo.com>
2020-11-03 14:27:02 +0800

    Update registration function

--- src/Register.js
+++ src/Register.js
@@ -5,11 +5,12 @@ import logo1 from "./assets/img/logo1.png";
 
 import IndexNavbar from "./components/Navbars/IndexNavbar2.js";
 import Footer from "./components/Footers/Footer";
+import swal from "sweetalert";
 
 function Register(props) {
 
   // const [error, setError]     = useState(null);
-  // const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [form, setForm] = useState({
     username: '',
     password: '',
@@ -26,6 +27,8 @@ function Register(props) {
 
   const handleRegister = (e) => {
     e.preventDefault();
+    setLoading(true);
+
     let formData = new FormData();
     formData.append('username',form.username);
     formData.append('password',form.password);
@@ -36,17 +39,19 @@ function Register(props) {
     .post('https://mymps.corrad.my/int/api_generator.php?api_name=daftar_pengguna',formData)
     .then(response => {
       console.log(response);
-      console.log('UserName:'+formData.get('username'));
-      console.log('Password:'+formData.get('password'));
-      console.log('Email:'+formData.get('email'));
-      console.log('No Kp:'+formData.get('nokp'));
+
+      if(response.data.status === 'success'){
+        // console.log('success');
+        swal('Pendaftaran Berjaya',"Something something something","success");
+        setLoading(false);
+      }
+      else{
+        // console.log('failure');
+        swal('Pendaftaran Tidak Berjaya',"Something something something","error");
+        setLoading(false);
+      }
     })
   };
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   console.log(form);
-  // }
 
   return (
     <div className="bg-gray">
@@ -89,7 +94,7 @@ function Register(props) {
 
                     <div className="col-span-6">
                       <label htmlFor="nokp" className="block text-sm font-medium leading-5 text-gray-700">NO KAD PENGENALAN</label>
-                      <input id="nokp" onChange={(e) => handleChange(e)} className="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                      <input id="nokp" onChange={(e) => handleChange(e)} className="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" maxLength="12" />
                     </div>
 
                     <div className="col-span-6">
@@ -120,4 +125,8 @@ function Register(props) {
     </div>
   );
 }
+
+// const handleValidation = e =>{
+  
+// }
 export default Register;
