b30c2eaf24cc7b2e6fca8e9ec3d7ac2932e6e61c
wanasyraf96 <wanasyraf96@yahoo.com>
2020-11-03 12:49:49 +0800

    Adding node module, register function
    Node module for npm
    
    adding registration function on Register.js

--- src/Register.js
+++ src/Register.js
@@ -7,43 +7,46 @@ import IndexNavbar from "./components/Navbars/IndexNavbar2.js";
 import Footer from "./components/Footers/Footer";
 
 function Register(props) {
-  const username = useFormInput("");
-  const password = useFormInput("");
-  const email = useFormInput("");
-  const nokp = useFormInput("");
 
-  const [error, setError]     = useState(null);
-  const [loading, setLoading] = useState(false);
+  // const [error, setError]     = useState(null);
+  // const [loading, setLoading] = useState(false);
+  const [form, setForm] = useState({
+    username: '',
+    password: '',
+    email: '',
+    nokp: ''
+  });
 
-  const handleRegister = () => {
-    setError(null);
-    setLoading(true);
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.id]: e.target.value
+    })
+  }
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    let formData = new FormData();
+    formData.append('username',form.username);
+    formData.append('password',form.password);
+    formData.append('email',form.email);
+    formData.append('nokp',form.nokp);
 
     axios
-      .post("https://apisim.mps.gov.my/api/mymps/akaunbyic?nokp=930420145231", {
-        username: username.value,
-        password: password.value,
-      })
-      .then((response) => {
-        setLoading(false);
-        setUserSession(response.data[0].NOAKAUN, response.data[0].NAMA_PEMILIK);
-
-        console.log(response.data);
-        props.history.push("/dashboard");
-      })
-      .catch((error) => {
-        setLoading(false);
-
-        console.log("Something went wrong. Plese try again!");
-        if (error.response.status === 401)
-          setError(error.response.data.message);
-        else setError("Something went wrong. Please try again later.");
-      });
+    .post('https://mymps.corrad.my/int/api_generator.php?api_name=daftar_pengguna',formData)
+    .then(response => {
+      console.log(response);
+      console.log('UserName:'+formData.get('username'));
+      console.log('Password:'+formData.get('password'));
+      console.log('Email:'+formData.get('email'));
+      console.log('No Kp:'+formData.get('nokp'));
+    })
   };
 
-  const handleNumber = () => {
-    console.log(username.value);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(form);
+  // }
 
   return (
     <div className="bg-gray">
@@ -53,51 +56,51 @@ function Register(props) {
       <section className="py-20 relative" style={{background: "rgb(34,81,122)", background: "linear-gradient(90deg, rgba(34,81,122,1) 0%, rgba(27,147,171,1) 100%)"}}>
 
       {/* sample form */}
-      <div class=" sm:mt-0" style={{padding: "20px"}}>
-        <div class="md:grid md:grid-cols-3 md:gap-6">
-          <div class="md:col-span-1 bg-gray-100" style={{padding: "20px", borderRadius: "10px"}}>
-            <div class="px-4 sm:px-0">
+      <div className=" sm:mt-0" style={{padding: "20px"}}>
+        <div className="md:grid md:grid-cols-3 md:gap-6">
+          <div className="md:col-span-1 bg-gray-100" style={{padding: "20px", borderRadius: "10px"}}>
+            <div className="px-4 sm:px-0">
               <img className="mx-auto" src={logo1} style={{width:"120px", height:"120px"}}/>
-              <h3 class="text-lg font-medium leading-6 text-gray-900 text-center" style={{marginTop: "30px"}}>Daftar Pengguna</h3>
-              <p class="mt-1 text-sm leading-5 text-gray-600" style={{marginTop: "30px"}}>
+              <h3 className="text-lg font-medium leading-6 text-gray-900 text-center" style={{marginTop: "30px"}}>Daftar Pengguna</h3>
+              <p className="mt-1 text-sm leading-5 text-gray-600" style={{marginTop: "30px"}}>
                 Selamat Datang ke laman daftar sebagai pengguna Majlis Perbandaran Selayang 2020. Sila pastikan setiap maklumat diisi lengkap sebelum daftar sebagai pengguna.
               </p>
             </div>
           </div>
-          <div class="mt-5 md:mt-0 md:col-span-2">
-            <form>
-              <div class="shadow overflow-hidden sm:rounded-md">
-                <div class="px-4 py-5 bg-white sm:p-6">
-                  <div class="grid grid-cols-6 gap-6">
-                    <div class="col-span-6 sm:col-span-3">
-                      <label for="first_name" class="block text-sm font-medium leading-5 text-gray-700">KATA PENGGUNA</label>
-                      <input id="first_name" class="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+          <div className="mt-5 md:mt-0 md:col-span-2">
+            <form onSubmit={(e) => handleRegister(e)}>
+              <div className="shadow overflow-hidden sm:rounded-md">
+                <div className="px-4 py-5 bg-white sm:p-6">
+                  <div className="grid grid-cols-6 gap-6">
+                    <div className="col-span-6 sm:col-span-3">
+                      <label htmlFor="username" className="block text-sm font-medium leading-5 text-gray-700">KATA PENGGUNA</label>
+                      <input id="username" onChange={(e) => handleChange(e)} className="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                     </div>
 
-                    <div class="col-span-6 sm:col-span-3">
-                      <label for="last_name" class="block text-sm font-medium leading-5 text-gray-700">KATA LALUAN</label>
-                      <input id="last_name" class="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                    <div className="col-span-6 sm:col-span-3">
+                      <label htmlFor="password" className="block text-sm font-medium leading-5 text-gray-700">KATA LALUAN</label>
+                      <input id="password" type='password' onChange={(e) => handleChange(e)} className="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                     </div>
 
-                    <div class="col-span-6 sm:col-span-6">
-                      <label for="email_address" class="block text-sm font-medium leading-5 text-gray-700">ALAMAT EMEL</label>
-                      <input id="email_address" class="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                    <div className="col-span-6 sm:col-span-6">
+                      <label htmlFor="email" className="block text-sm font-medium leading-5 text-gray-700">ALAMAT EMEL</label>
+                      <input id="email" onChange={(e) => handleChange(e)} className="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                     </div>
 
-                    <div class="col-span-6">
-                      <label for="street_address" class="block text-sm font-medium leading-5 text-gray-700">NO KAD PENGENALAN</label>
-                      <input id="street_address" class="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                    <div className="col-span-6">
+                      <label htmlFor="nokp" className="block text-sm font-medium leading-5 text-gray-700">NO KAD PENGENALAN</label>
+                      <input id="nokp" onChange={(e) => handleChange(e)} className="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                     </div>
 
-                    <div class="col-span-6">
-                      <a href="/login" class="font-small text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
-                        <i class="fas fa-user"></i> Sudah mempunyai akaun? Login
+                    <div className="col-span-6">
+                      <a href="/login" className="font-small text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+                        <i className="fas fa-user"></i> Sudah mempunyai akaun? Login
                       </a>
                     </div>
                   </div>
                 </div>
-                <div class="px-4 py-3 bg-gray-50 text-right sm:px-6 bg-gray-300">
-                  <button type="button" class="py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-green-600 shadow-sm hover:bg-green-500 focus:outline-none focus:shadow-outline-blue active:bg-indigo-600 transition duration-150 ease-in-out">
+                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6 bg-gray-300">
+                  <button type="submit" className="py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-green-600 shadow-sm hover:bg-green-500 focus:outline-none focus:shadow-outline-blue active:bg-indigo-600 transition duration-150 ease-in-out">
                     Daftar
                   </button>
                 </div>
@@ -117,17 +120,4 @@ function Register(props) {
     </div>
   );
 }
-
-const useFormInput = (initialValue) => {
-  const [value, setValue] = useState(initialValue);
-
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
-  return {
-    value,
-    onChange: handleChange,
-  };
-};
-
 export default Register;
